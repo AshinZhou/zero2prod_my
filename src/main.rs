0a8eb583a5a221007aa50e4d8dab3c59f09b2dad
Ashin <zhouaijun0214@foxmail.com>
2025-04-17 08:34:47 +0800

    docker-1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use secrecy::ExposeSecret;
 use zero2prod_my::configuration::get_configuration;
 use zero2prod_my::startup::run;
 use zero2prod_my::telemetry::{get_subscriber, init_subscriber};
@@ -11,12 +11,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     let configuration = get_configuration().expect("Failed to read configuration");
     let configuration_string = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&configuration_string.expose_secret())
-        .await
-        .expect("Failed to connect Postgres.");
+    let connection_pool = PgPool::connect_lazy(&configuration_string.expose_secret())
+        .expect("Failed to create Postgres connection pool.");
     run(listener, connection_pool)?.await
 }
